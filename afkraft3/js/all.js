3106caedfd9e2734f37fa12246fb9fd8cbdffa70
ProkinaT <tgprokina@edu.hse.ru>
2022-03-10 23:09:50 +0300

    Create all.js

---
+++ afkraft3/js/all.js
@@ -0,0 +1,385 @@
+
+// маска
+$(document).ready(function(){
+  $(".mask").click(function(){
+    if ($(".mask").hasClass("mask10")){
+      $(".mask").addClass("mask1");
+      $(".head").addClass("mask1");
+      setTimeout(function(){
+        $(".mask").removeClass("mask10");
+      $(".head").removeClass("mask10");
+    }, 100);
+  } else if ( $(".mask").hasClass("mask1")){
+      $(".mask").addClass("mask2");
+      $(".head").addClass("mask2");
+      setTimeout(function(){
+        $(".mask").removeClass("mask1");
+        $(".head").removeClass("mask1");
+    }, 100);
+  } else if ( $(".mask").hasClass("mask2")){
+      $(".mask").addClass("mask3");
+      $(".head").addClass("mask3");
+      setTimeout(function(){
+        $(".mask").removeClass("mask2");
+      $(".head").removeClass("mask2");
+    }, 100);
+	} else if ( $(".mask").hasClass("mask3")){
+			$(".mask").addClass("mask4");
+			$(".head").addClass("mask4");
+			setTimeout(function(){
+				$(".mask").removeClass("mask3");
+			$(".head").removeClass("mask3");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask4")){
+			$(".mask").addClass("mask5");
+			$(".head").addClass("mask5");
+			setTimeout(function(){
+				$(".mask").removeClass("mask4");
+			$(".head").removeClass("mask4");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask5")){
+			$(".mask").addClass("mask6");
+			$(".head").addClass("mask6");
+			setTimeout(function(){
+				$(".mask").removeClass("mask5");
+			$(".head").removeClass("mask5");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask6")){
+			$(".mask").addClass("mask7");
+			$(".head").addClass("mask7");
+			setTimeout(function(){
+				$(".mask").removeClass("mask6");
+			$(".head").removeClass("mask6");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask7")){
+			$(".mask").addClass("mask8");
+			$(".head").addClass("mask8");
+			setTimeout(function(){
+				$(".mask").removeClass("mask7");
+			$(".head").removeClass("mask7");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask8")){
+			$(".mask").addClass("mask9");
+			$(".head").addClass("mask9");
+			setTimeout(function(){
+				$(".mask").removeClass("mask8");
+			$(".head").removeClass("mask8");
+		}, 100);
+	} else if ( $(".mask").hasClass("mask9")){
+			$(".mask").addClass("mask10");
+			$(".head").addClass("mask10");
+			setTimeout(function(){
+				$(".mask").removeClass("mask9");
+			$(".head").removeClass("mask9");
+		}, 100);
+    };
+  });
+});
+// глаза
+$(document).ready(function(){
+  $(".eyes").click(function(){
+    if ($(".eyes").hasClass("y1")){
+      $(".eyes").addClass("y2");
+      $(".eyes1").addClass("y2");
+      setTimeout(function(){
+        $(".eyes").removeClass("y1");
+      $(".eyes1").removeClass("y1");
+    }, 100);
+  } else if ( $(".eyes").hasClass("y2")){
+      $(".eyes").addClass("y3");
+      $(".eyes1").addClass("y3");
+      setTimeout(function(){
+        $(".eyes").removeClass("y2");
+        $(".eyes1").removeClass("y2");
+    }, 100);
+  } else if ( $(".eyes").hasClass("y3")){
+      $(".eyes").addClass("y4");
+      $(".eyes1").addClass("y4");
+      setTimeout(function(){
+        $(".eyes").removeClass("y3");
+      $(".eyes1").removeClass("y3");
+    }, 100);
+	} else if ( $(".eyes").hasClass("y4")){
+			$(".eyes").addClass("y5");
+			$(".eyes1").addClass("y5");
+			setTimeout(function(){
+				$(".eyes").removeClass("y4");
+			$(".eyes1").removeClass("y4");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y5")){
+			$(".eyes").addClass("y6");
+			$(".eyes1").addClass("y6");
+			setTimeout(function(){
+				$(".eyes").removeClass("y5");
+			$(".heyes1").removeClass("y5");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y6")){
+			$(".eyes").addClass("y7");
+			$(".eyes1").addClass("y7");
+			setTimeout(function(){
+				$(".eyes").removeClass("y6");
+			$(".eyes1").removeClass("y6");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y7")){
+			$(".eyes").addClass("y8");
+			$(".eyes1").addClass("y8");
+			setTimeout(function(){
+				$(".eyes").removeClass("y7");
+			$(".eyes1").removeClass("y7");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y8")){
+			$(".eyes").addClass("y9");
+			$(".eyes1").addClass("y9");
+			setTimeout(function(){
+				$(".eyes").removeClass("y8");
+			$(".eyes1").removeClass("y8");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y9")){
+			$(".eyes").addClass("y10");
+			$(".eyes1").addClass("y10");
+			setTimeout(function(){
+				$(".eyes").removeClass("y9");
+			$(".eyes1").removeClass("y9");
+		}, 100);
+	} else if ( $(".eyes").hasClass("y10")){
+			$(".eyes").addClass("y1");
+			$(".eyes1").addClass("y1");
+			setTimeout(function(){
+				$(".eyes").removeClass("y10");
+			$(".eyes1").removeClass("y10");
+		}, 100);
+    };
+  });
+});
+// нос
+$(document).ready(function(){
+  $(".nose").click(function(){
+    if ($(".nose").hasClass("n1")){
+      $(".nose").addClass("n2");
+      $(".nose1").addClass("n2");
+      setTimeout(function(){
+        $(".nose").removeClass("n1");
+      $(".nose1").removeClass("n1");
+    }, 100);
+  } else if ( $(".nose").hasClass("n2")){
+      $(".nose").addClass("n3");
+      $(".nose1").addClass("n3");
+      setTimeout(function(){
+        $(".nose").removeClass("n2");
+        $(".nose1").removeClass("n2");
+    }, 100);
+  } else if ( $(".nose").hasClass("n3")){
+      $(".nose").addClass("n4");
+      $(".nose1").addClass("n4");
+      setTimeout(function(){
+        $(".nose").removeClass("n3");
+      $(".nose1").removeClass("n3");
+    }, 100);
+	} else if ( $(".nose").hasClass("n4")){
+			$(".nose").addClass("n5");
+			$(".nose1").addClass("n5");
+			setTimeout(function(){
+				$(".nose").removeClass("n4");
+			$(".nose1").removeClass("n4");
+		}, 100);
+	} else if ( $(".nose").hasClass("n5")){
+			$(".nose").addClass("n6");
+			$(".nose1").addClass("n6");
+			setTimeout(function(){
+				$(".nose").removeClass("n5");
+			$(".nose1").removeClass("n5");
+		}, 100);
+	} else if ( $(".nose").hasClass("n6")){
+			$(".nose").addClass("n7");
+			$(".nose1").addClass("n7");
+			setTimeout(function(){
+				$(".nose").removeClass("n6");
+			$(".nose1").removeClass("n6");
+		}, 100);
+	} else if ( $(".nose").hasClass("n7")){
+			$(".nose").addClass("n8");
+			$(".nose1").addClass("n8");
+			setTimeout(function(){
+				$(".nose").removeClass("n7");
+			$(".nose1").removeClass("n7");
+		}, 100);
+	} else if ( $(".nose").hasClass("n8")){
+			$(".nose").addClass("n9");
+			$(".nose1").addClass("n9");
+			setTimeout(function(){
+				$(".nose").removeClass("n8");
+			$(".nose1").removeClass("n8");
+		}, 100);
+	} else if ( $(".nose").hasClass("n9")){
+			$(".nose").addClass("n10");
+			$(".nose1").addClass("n10");
+			setTimeout(function(){
+				$(".nose").removeClass("n9");
+			$(".nose1").removeClass("n9");
+		}, 100);
+	} else if ( $(".nose").hasClass("n10")){
+			$(".nose").addClass("n1");
+			$(".nose1").addClass("n1");
+			setTimeout(function(){
+				$(".nose").removeClass("n10");
+			$(".nose1").removeClass("n10");
+		}, 100);
+    };
+  });
+});
+// рот
+$(document).ready(function(){
+  $(".mouth").click(function(){
+    if ($(".mouth").hasClass("m1")){
+      $(".mouth").addClass("m2");
+      $(".mouth1").addClass("m2");
+      setTimeout(function(){
+        $(".mouth").removeClass("m1");
+      $(".mouth1").removeClass("m1");
+    }, 100);
+  } else if ( $(".mouth").hasClass("m2")){
+      $(".mouth").addClass("m3");
+      $(".mouth1").addClass("m3");
+      setTimeout(function(){
+        $(".mouth").removeClass("m2");
+        $(".mouth1").removeClass("m2");
+    }, 100);
+  } else if ( $(".mouth").hasClass("m3")){
+      $(".mouth").addClass("m4");
+      $(".mouth1").addClass("m4");
+      setTimeout(function(){
+        $(".mouth").removeClass("m3");
+      $(".mouth1").removeClass("m3");
+    }, 100);
+	} else if ( $(".mouth").hasClass("m4")){
+			$(".mouth").addClass("m5");
+			$(".mouth1").addClass("m5");
+			setTimeout(function(){
+				$(".mouth").removeClass("m4");
+			$(".mouth1").removeClass("m4");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m5")){
+			$(".mouth").addClass("m6");
+			$(".mouth1").addClass("m6");
+			setTimeout(function(){
+				$(".mouth").removeClass("m5");
+			$(".mouth1").removeClass("m5");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m6")){
+			$(".mouth").addClass("m7");
+			$(".mouth1").addClass("m7");
+			setTimeout(function(){
+				$(".mouth").removeClass("m6");
+			$(".mouth1").removeClass("m6");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m7")){
+			$(".mouth").addClass("m8");
+			$(".mouth1").addClass("m8");
+			setTimeout(function(){
+				$(".mouth").removeClass("m7");
+			$(".mouth1").removeClass("m7");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m8")){
+			$(".mouth").addClass("m9");
+			$(".mouth1").addClass("m9");
+			setTimeout(function(){
+				$(".mouth").removeClass("m8");
+			$(".mouth1").removeClass("m8");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m9")){
+			$(".mouth").addClass("m10");
+			$(".mouth1").addClass("m10");
+			setTimeout(function(){
+				$(".mouth").removeClass("m9");
+			$(".mouth1").removeClass("m9");
+		}, 100);
+	} else if ( $(".mouth").hasClass("m10")){
+			$(".mouth").addClass("m1");
+			$(".mouth1").addClass("m1");
+			setTimeout(function(){
+				$(".mouth").removeClass("m10");
+			$(".mouth1").removeClass("m10");
+		}, 100);
+    };
+  });
+});
+
+$( function() {
+    $(".hat" ).draggable();
+  } );
+$( function() {
+    $(".glines" ).draggable();
+  } );
+$( function() {
+    $(".veer" ).draggable();
+  } );
+$( function() {
+    $(".cube" ).draggable();
+  } );
+$( function() {
+    $(".oval" ).draggable();
+  } );
+$( function() {
+    $(".whtriangle" ).draggable();
+  } );
+$( function() {
+    $(".half" ).draggable();
+  } );
+$( function() {
+    $(".boldpoloski" ).draggable();
+  } );
+$( function() {
+    $(".pavlin" ).draggable();
+  } );
+$( function() {
+    $(".rain" ).draggable();
+  } );
+$( function() {
+    $(".zigzag" ).draggable();
+  } );
+$( function() {
+    $(".scratchesgor" ).draggable();
+  } );
+$( function() {
+    $(".pero" ).draggable();
+  } );
+$( function() {
+    $(".zigzagb" ).draggable();
+  } );
+$( function() {
+    $(".voidtriangle" ).draggable();
+  } );
+$( function() {
+    $(".scratch" ).draggable();
+  } );
+$( function() {
+    $(".plane" ).draggable();
+  } );
+$( function() {
+    $(".circlegreen" ).draggable();
+  } );
+
+  $(function() {
+  $('.next').click(function() {
+    $(".site").animate({
+      scrollTop:1800
+    },1000);
+    setTimeout(function(){
+      $(".head, .eyes1, .nose1, .mouth1, .patterns").addClass("op");
+  }, 100);
+  })
+})
+$(function(){
+    $(".restart").bind('click', function(e){
+        $(".handl").addClass("handl1");
+        $(".handc").addClass("handc1");
+        $(".handr").addClass("handr1");
+        e.preventDefault();
+        $('body,html').animate({scrollTop: 0}, 400);
+    });
+});
+// $(document).ready(function(){
+//   $(".mask").click(function(){
+//     if ($(".mask").hasClass("circlegreen")){
+//       $(".patterns").addClass("circlegreen");
+//     },);
