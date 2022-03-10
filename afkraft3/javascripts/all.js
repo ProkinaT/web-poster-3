aa74116da372db06fe7828c95c3544803d3b5da9
ProkinaT <tgprokina@edu.hse.ru>
2022-03-10 23:09:45 +0300

    Create all.js

---
+++ afkraft3/javascripts/all.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+	$('.mask mask10').click(function () {
+		$(this).removeClass('.mask10');
+		});
+	});
+// $(document).ready(function(){
+//   $(".mask").click(function(){
+//     if ($(".mask").hasClass("mask10")){
+//       $(".mask").addClass("mask9");
+//       // $(".hairv").addClass("pur");
+//       // setTimeout(function(){
+//       //   $(".mask").removeClass("mask9");
+//       // $(".hairv").removeClass("green");
+//     }, 100);
+//   } else if ( $(".mask").hasClass("mask10")){
+//       $(".mask").addClass("mask9");
+//       // $(".hairv").addClass("green");
+//       setTimeout(function(){
+//         $(".mask").removeClass("mask10");
+//         // $(".hairv").removeClass("pinkk");
+//     }, 100);
+//   } else if ( $(".").hasClass("one")){
+//       $(".hair").addClass("two");
+//       $(".hairv").addClass("pinkk");
+//       setTimeout(function(){
+//         $(".hair").removeClass("one");
+//       $(".hairv").removeClass("pur");
+//     }, 100);
+//     };
+//   });
+// });
